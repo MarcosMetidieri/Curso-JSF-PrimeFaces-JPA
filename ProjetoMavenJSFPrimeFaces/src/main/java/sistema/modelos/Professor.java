81af7dfd686ed8a321690c4429ae02c5f5168dfc
0030481621031 <0030481621031@LTI6302.lti.fatecso.edu.br>
2018-05-14 12:15:50 -0300

    Cadastro de Professor

---
+++ ProjetoMavenJSFPrimeFaces/src/main/java/sistema/modelos/Professor.java
@@ -0,0 +1,39 @@
+package sistema.modelos;
+
+import java.io.Serializable;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Professor implements Serializable {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int numRegistro;
+	
+	private String nome;
+	private String disciplina;
+	
+	public int getNumRegistro() {
+		return numRegistro;
+	}
+	public void setNumRegistro(int numRegistro) {
+		this.numRegistro = numRegistro;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDisciplina() {
+		return disciplina;
+	}
+	public void setDisciplina(String disciplina) {
+		this.disciplina = disciplina;
+	}
+}
